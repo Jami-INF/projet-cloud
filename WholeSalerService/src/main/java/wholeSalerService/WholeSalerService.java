a8c92756cfa1b53555e3664b9ddce5522ea895f9
Jami infante <jami@MacBook.lan>
2023-05-03 23:33:05 +0200

    ADD : wholesaler service OK

---
+++ WholeSalerService/src/main/java/wholeSalerService/WholeSalerService.java
@@ -0,0 +1,33 @@
+package wholeSalerService;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+@Path("wholesalerservice")
+public class WholeSalerService {
+
+    @GET
+    @Path("order")
+    @Produces("text/plain")
+    public Response order(@QueryParam("isbn") String isbn){
+        Response response = null;
+
+        try {
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target("https://stock-service.herokuapp.com").path("stockservice/increase/").queryParam("isbn", isbn);
+            response = target.request().get();
+        } catch (Exception e) {
+            return Response.status(500).entity("Impossible to execute the query " + e.getMessage()).build();
+        }
+
+        if(response.getStatus() == 200){
+            return Response.status(200).entity("Order done").build();
+        }else{
+            return Response.status(500).entity("Impossible to execute the query " + response.getStatus()).build();
+        }
+    }
+}
